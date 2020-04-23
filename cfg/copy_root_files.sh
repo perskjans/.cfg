7c5f166346fb107fe4bc680b9d3c8da1f0e11c2b
Per Erskjans <per.erskjans@gmail.com>
2020-04-23 20:16:40 +0200

    keyboard.

--- cfg/copy_root_files.sh
+++ cfg/copy_root_files.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-sudo cp $HOME/cfg/rootfiles/*.desktop /usr/share/applications/
+#sudo cp $HOME/cfg/rootfiles/*.desktop /usr/share/applications/
+sudo cp -f $HOME/cfg/rootfiles/keyboard.layout /usr/share/X11/xkb/symbols/custom
 
