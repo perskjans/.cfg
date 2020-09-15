abacfafa32b4948874a0ca4106388d8b40ec9c51
Per Erskjans <per.erskjans@gmail.com>
2020-09-15 20:10:39 +0200

    Keyboard: Changes to layout.

---
+++ cfg/link_root_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#sudo cp $HOME/cfg/rootfiles/*.desktop /usr/share/applications/
+#sudo cp -f $HOME/cfg/rootfiles/keyboard.layout /usr/share/X11/xkb/symbols/custom
+sudo ln -s -f $HOME/cfg/rootfiles/keyboard.layout /usr/share/X11/xkb/symbols/custom
+
